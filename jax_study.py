7e19ae14ecdfe78610f9c478b57714f29d39a00b
Pedro Jorge <pmrjge@gmail.com>
2023-08-14 00:02:50 +0100

    Examples of automatic differentiation like numpy's Autograd using JAX grad and comparing results to first order finite differences, besides coded how to compute the hessian using jax reverse and forward vector-jacobian

--- jax_study.py
+++ jax_study.py
@@ -1,5 +1,5 @@
 import jax.numpy as jnp
-from jax import grad, jit, vmap, random, device_put
+from jax import grad, jit, vmap, random, device_put, jacfwd, jacrev
 from timeit import default_timer as timer
 
 # Generate random data in the following examples
@@ -45,4 +45,25 @@ ti = timer()
 selu_jit(x).block_until_ready()
 tf = timer()
 print(f"it took {tf - ti} ms to execute code in jax with jit")
+
+
+# Taking derivatives with grad()
+
+def sum_logistic(x):
+    return jnp.sum(1.0 / (1.0 + jnp.exp(-x)))
+
+x_small = jnp.arange(3.)
+derivative_fn = grad(sum_logistic)
+print(derivative_fn(x_small))
+
+def first_finite_differences(f, x):
+    eps = 1e-3
+    return jnp.array([(f(x + eps * v) - f(x - eps * v)) / (2 * eps) for v in jnp.eye(len(x))])
+
+print(first_finite_differences(sum_logistic, x_small))
+
+print(grad(jit(grad(jit(grad(sum_logistic)))))(1.0))
+
+def hessian(f):
+    return jit(jacfwd(jacrev(f)))
 
