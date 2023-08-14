dcc0b57bcedc419ea918576dd6cf8dd8a921b746
Pedro Jorge <pmrjge@gmail.com>
2023-08-14 17:16:20 +0100

    Fixed errors introduced on typos or bad-reading; jit vs non jitted functions

--- jax_study.py
+++ jax_study.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
-from jax import grad, jit, vmap, random, device_put, jacfwd, jacrev
+from jax import grad, jit, vmap, random, device_put, jacfwd, jacrev, lax
 from timeit import default_timer as timer
+import matplotlib.pyplot as plt
 
 # Generate random data in the following examples
 
@@ -104,3 +105,50 @@ t0 = timer()
 vmap_batched_apply_matrix(batched_x).block_until_ready()
 t1 = timer()
 print(f'it took {t1 - t0} ms.')
+
+# JAX vs Numpy
+## JAX arrays are immutable
+x_np = np.linspace(0, 10, 1000)
+y_np = 2 * np.sin(x_np) * np.cos(x_np)
+plt.plot(x_np, y_np)
+
+x_jnp = jnp.linspace(0, 10, 1000)
+y_jnp = 2 * jnp.sin(x_jnp) * jnp.cos(x_jnp)
+plt.plot(x_jnp, y_jnp)
+
+
+## Numpy, lax & XLA
+
+print(jnp.add(1, 1.0))
+print(lax.add(jnp.float32(1), 1.0))
+
+x = jnp.array([ 1, 2, 1])
+y = jnp.ones(10)
+
+print(jnp.convolve(x, y))
+
+result = lax.conv_general_dilated(x.reshape(1, 1, 3).astype(float), y.reshape(1, 1, 10), window_strides=(1,), padding="same")
+
+print(result[0, 0])
+
+## JIT or not to JIT
+
+def norm(X):
+    X = X - X.mean(0)
+    return X / X.std(0)
+
+norm_compiled = jit(norm)
+
+np.random.seed(1701)
+X = jnp.array(np.random.rand(10000, 10))
+print(np.allclose(norm(X), norm_compiled(X), atol=1E-6))
+
+t0 = timer()
+norm(X).block_until_ready()
+t1 = timer()
+print(f"Time using non jitted function: {t1 - t0} ms")
+
+t0 = timer()
+norm_compiled(X).block_until_ready()
+t1 = timer()
+print(f"Time using jitted function: {t1 - t0} ms")
